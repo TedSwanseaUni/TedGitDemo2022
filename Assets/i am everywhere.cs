6c3e332eaccae50f952129723eee79cc02cc68aa
02Dhruvshah <115563257+02Dhruvshah@users.noreply.github.com>
2022-10-11 16:46:01 +0100

    guess who

---
+++ Assets/i am everywhere.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iameverywhere : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //GUess who
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
